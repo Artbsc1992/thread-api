3e01bd7a05efa8e63dcb37d35130547348a080a5
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-11-01 11:27:56 -0600

    Update friendships and users relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :friendships_as_user, 
+      foreign_key: :user_id, 
+      class_name: :Friendship
+  has_many :friends, through: :friendships_as_user
 end
