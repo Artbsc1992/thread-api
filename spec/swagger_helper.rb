7f1903b6b5623db762e97ff37df589d328fbb211
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-11-01 10:55:16 -0600

    Change port 4000 to 3000 in config files

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -33,7 +33,7 @@ RSpec.configure do |config|
           url: 'http://{defaultHost}',
           variables: {
             defaultHost: {
-              default: 'localhost:4000'
+              default: 'localhost:3000'
             }
           }
         }
