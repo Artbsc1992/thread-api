ab5d768153beec7e645a65e24b5d606937655b64
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2022-11-01 11:39:13 -0600

    Update notifications model and migration
    Notifications now belong to a user and a daily

--- db/migrate/20221101164220_create_notifications.rb
+++ db/migrate/20221101164220_create_notifications.rb
@@ -3,6 +3,8 @@ class CreateNotifications < ActiveRecord::Migration[7.0]
     create_table :notifications do |t|
       t.text :message
       t.boolean :viewed
+      t.references :user_id, null: false, foreign_key: { to_table: :users }
+      t.references :daily_id, null: false, foreign_key: { to_table: :dailies }
 
       t.timestamps
     end
