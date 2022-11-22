33dfd1e46a2dfa2c058e20a13a90ddfae7ca95a4
arturo <ahpareja@gmail.com>
2022-11-22 11:26:56 -0500

    Add notifications validations

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,9 @@
 class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :daily
+
+  validates :user_id, presence: true
+  validates :daily_id, presence: true
+  validates :message, presence: true, length: { maximum: 31 }
+  validates :viewed, inclusion: { in: [true, false] }, default: false
 end
