bbf55bf0b9fb2a327118efa26257ebbe675e1ae1
arturo <ahpareja@gmail.com>
2022-11-22 11:26:22 -0500

    Add Daily validations

--- app/models/daily.rb
+++ app/models/daily.rb
@@ -1,2 +1,8 @@
 class Daily < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :notifications, dependent: :destroy
+
+  validates :content, presence: true, length: { maximum: 255 }
+  validates :user_id, presence: true
 end
