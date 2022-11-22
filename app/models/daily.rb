cd5c215d55217f78af13446da398e4d3d8ddcab3
arturo <ahpareja@gmail.com>
2022-11-22 15:15:25 -0500

    create show_relevant method

--- app/models/daily.rb
+++ app/models/daily.rb
@@ -5,4 +5,6 @@ class Daily < ApplicationRecord
 
   validates :content, presence: true, length: { maximum: 255 }
   validates :user_id, presence: true
+  # para mostrar dailies por su relevancia
+  # scope :show_relevant, ->(current_user) { where(user_id: User.where(role: 'admin') + current_user.friends.pluck(:id) << current_user.id) }
 end
