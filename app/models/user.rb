759f64f89f699a98149d296f1d28affc45ee753a
arturo <ahpareja@gmail.com>
2022-11-22 11:27:03 -0500

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-  has_many :friendships_as_user, 
+  has_many :friendships, 
       foreign_key: :user_id, 
       class_name: :Friendship
-  has_many :friends, through: :friendships_as_user
+  has_many :friends, through: :friendships
+
+  validates :name, presence: true, length: { maximum: 63 }
+  validates :bio, length: { maximum: 127 }
+  validates :email, presence: true, length: { maximum: 63 }, uniqueness: true, format { with: URI::MailTo::EMAIL_REGEXP }
+  validates :photo, format { with: URI.regexp }, allow_blank: true
 end
