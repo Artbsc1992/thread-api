77a51655a2647c77ceb8300946e8e8381c4e35c5
arturo <ahpareja@gmail.com>
2022-11-22 11:26:48 -0500

    Add friendship validations

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,15 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: :User
+
+  validates :user_id, presence: true, uniqueness: { scope: :friend_id }
+  validates :friend_id, presence: true
+  validate :cannot_add_self
+
+  private
+
+  def cannot_add_self
+    errors.add(:friend_id, 'You cannot add yourself as a friend.') if user_id == friend_id
+  end
+
 end
