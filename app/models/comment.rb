cdc0af634b059de96737c50c7dd3fc6cd4db855f
arturo <ahpareja@gmail.com>
2022-11-22 11:26:12 -0500

    Add commnets validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,9 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :daily
+  has_many :notifications, dependent: :destroy
+
+  validates :content, presence: true, length: { maximum: 127 }
+  validates :user_id, presence: true
+  validates :daily_id, presence: true
 end
